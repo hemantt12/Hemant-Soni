30dd751c40f08ff16ddeb038f1981dd457ed7020
hemantt12 <100987217+hemantt12@users.noreply.github.com>
2022-06-02 17:44:10 +0530

    How to concatenate multiple dictionary in one

---
+++ dict8.py
@@ -0,0 +1,11 @@
+Fruit = {'Apple': 100, 'banana': 5}
+ 
+Subject =  {'Math': 100, 'English': 98} 
+ 
+animal =  {'name': 'Rabbit', 'age': 1}
+ 
+conacte_Dict =  {}
+ 
+for dict in (Fruit, Subject, animal): conacte_Dict.update(dict)
+ 
+print(conacte_Dict)
