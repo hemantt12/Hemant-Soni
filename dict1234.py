9476805bd96fbf25730571de2d9453e16fb70161
hemantt12 <100987217+hemantt12@users.noreply.github.com>
2022-06-02 17:41:20 +0530

    How to change the name of key in dictionary.

---
+++ dict1234.py
@@ -0,0 +1,12 @@
+animaldict = {
+  "name": "Dog",
+  "age":5,
+  "Weight": 4,
+  "Country": "US",
+  "City":"California"
+   
+}
+ 
+animaldict['Animalname'] = animaldict.pop('name')
+ 
+print(animaldict)
