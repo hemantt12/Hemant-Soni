807b618984e767ec550928b22d6aebf3484fae8d
hemantt12 <100987217+hemantt12@users.noreply.github.com>
2022-06-02 17:47:09 +0530

    .How to convert dictionary object into string

---
+++ dict12.py
@@ -0,0 +1,11 @@
+personDict = { "Name" : "Simulen", 
+          "City" : "CA", 
+          "Country" : "USA"} 
+ 
+print ('Type before conversion = ',type(personDict)) 
+ 
+ 
+dicttoString = str(personDict) 
+ 
+print ("\ntype after conversion = ", type(dicttoString)) 
+print ("\ndict object to string = ", dicttoString) 
