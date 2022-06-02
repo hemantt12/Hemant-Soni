44f30d08ce62c5f482ccd091cbcd32a0a1663fe3
hemantt12 <100987217+hemantt12@users.noreply.github.com>
2022-06-02 17:47:57 +0530

    Program to add multiple keys into dictionary

---
+++ dict 12.py
@@ -0,0 +1,7 @@
+personDict = {'person':{ "Name" : "Simulen", 
+          "City" : "CA", 
+          "Country" : "USA"}}
+personDict['person']['Company'] = 'Morgan stanely'
+   
+  
+print("Dictionary after adding key =\n" ,personDict) 
